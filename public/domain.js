a01163d96de97951db63af93df4cb4a58ff66544
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-05-08 02:25:26 +0530

    first commit

---
+++ public/domain.js
@@ -0,0 +1,5 @@
+const domains = new Map([
+    ["universal indian", ["panipuri", "samosa", "chaat"]],
+    ["north indian", ["chole bathure", "tawa chicken", "kulcha"]],
+    ["fast food", ["pizza", "sandwich", "burger"]],
+  ]);
---
+++ public/script.js
@@ -0,0 +1,31 @@
+// const domains = require ('./public/domain');
+
+const domains = new Map([
+  ["universal indian", ["panipuri", "samosa", "chaat"]],
+  ["north indian", ["chole bathure", "tawa chicken", "kulcha"]],
+  ["fast food", ["pizza", "sandwich", "burger"]],
+]);
+
+const domainSubCategory = () => {
+  const domain = document.getElementById("Domain").value;
+  const domainItems = document.getElementById("domainItems");
+  while (domainItems.hasChildNodes()) {
+    domainItems.removeChild(domainItems.firstChild);
+  }
+  if (domain !== "Choose...") {
+    const categories = domains.get(domain);
+    console.log(categories);
+    categories.forEach((e) => {
+      const div = document.createElement("div");
+      const label = document.createElement("LABEL");
+      label.innerText = e;
+      const input = document.createElement("INPUT");
+      input.setAttribute("type", "checkbox");
+      input.setAttribute("name", e);
+      input.setAttribute("class", "checkbox");
+      div.appendChild(label);
+      div.appendChild(input);
+      domainItems.appendChild(div);
+    });
+  }
+};
